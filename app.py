c35ac91c46f157988ca01ddeb737b020d8db480f
Domenico Gaudioso <43296235+DomenicoGaudioso@users.noreply.github.com>
2025-01-27 10:49:10 +0000

    Add verifica a taglio

--- app.py
+++ app.py
@@ -24,6 +24,42 @@ Sollecitazioni = {'G1+':{'N': -0.0, 'T': 0.0, 'Mf': 9.58, 'Mt': 0.0}, 'G1-':{ 'N
                   'V+':{ 'N': 0.0, 'T': 0.0, 'Mf': 10.0, 'Mt': 0.0}, 'V-':{ 'N': 0.0, 'T': 0.0, 'Mf': 1200.0, 'Mt': 0.0},   # vento
         }
 
+
+def Resistenza_Piolo(d, ft, fck, Ec, hsc, gamma_v= 1.25):
+    """
+    Calcola la resistenza ultima di calcolo a taglio (SLU) di un piolo.
+
+    Parametri:
+    - d (float): Diametro del gambo del piolo (16 ≤ d ≤ 25 mm).
+    - ft (float): Resistenza a rottura dell'acciaio del piolo (≤ 500 MPa).
+    - fck (float): Resistenza caratteristica del calcestruzzo (MPa).
+    - Ec (float): Modulo elastico del calcestruzzo (MPa).
+    - hsc (float): Altezza del piolo dopo la saldatura.
+    - gamma_v (float): Fattore parziale di sicurezza del materiale (default: 1.25).
+
+    Restituisce:
+    - P_Rd (float): Resistenza ultima di calcolo a taglio (SLU).
+    """
+    if not (16 <= d <= 25):
+        raise ValueError("Il diametro d deve essere compreso tra 16 e 25 mm.")
+    if ft > 500:
+        raise ValueError("La resistenza a rottura dell'acciaio ft non deve superare 500 MPa.")
+    
+    # Calcolo del coefficiente α
+    if 3 <= hsc / d <= 4:
+        alpha = 0.2 * (hsc / d + 1)
+    elif hsc / d > 4:
+        alpha = 1.0
+    else:
+        raise ValueError("Il rapporto hsc/d deve essere ≥ 3.")
+    
+    # Calcolo dei due valori di resistenza
+    P_Rd_a = (0.8 * ft * (pi * d**2 / 4)) / (gamma_v*1000)
+    P_Rd_c = (0.29 * alpha * d**2 * sqrt(fck * Ec)) / (gamma_v*1000)
+    
+    # Restituisce il valore minimo
+    return (P_Rd_a, P_Rd_c)
+
 def combinazione(list_sigma, category = "A1_sfav"):
    
    path = "coefficienti.xlsx"
@@ -44,6 +80,7 @@ def combinazione(list_sigma, category = "A1_sfav"):
    sigma_slu = (np.array(list_sigma[0])*gamma_g1 #G1
    + np.array(list_sigma[1])*gamma_g2 #G2
    + np.array(list_sigma[2])*gamma_r #Ritiro
+
    + np.array(list_sigma[4])*gamma_m #Carichi mobili concentrati
    + np.array(list_sigma[5])*gamma_m #Carichi mobili distribuiti
    + np.array(list_sigma[6])*gamma_m #Carico folla
@@ -54,6 +91,7 @@ def combinazione(list_sigma, category = "A1_sfav"):
    sigma_rara = (np.array(list_sigma[0])*1 #G1
    + np.array(list_sigma[1])*1 #G2
    + np.array(list_sigma[2])*1 #Ritiro
+
    + np.array(list_sigma[4])*psi.loc['tandem', "psi0"]#Carichi mobili concentrati
    + np.array(list_sigma[5])*psi.loc['distribuiti', "psi0"] #Carichi mobili distribuiti
    + np.array(list_sigma[6])*0 #Carico folla
@@ -64,7 +102,8 @@ def combinazione(list_sigma, category = "A1_sfav"):
    sigma_frequente = (np.array(list_sigma[0])*1 #G1
    + np.array(list_sigma[1])*1 #G2
    + np.array(list_sigma[2])*1 #Ritiro
-   + np.array(list_sigma[3])*psi.loc['tandem', "psi1"]#Carichi mobili concentrati
+
+   + np.array(list_sigma[4])*psi.loc['tandem', "psi1"]#Carichi mobili concentrati
    + np.array(list_sigma[5])*psi.loc['distribuiti', "psi1"] #Carichi mobili distribuiti
    + np.array(list_sigma[6])*psi.loc['folla', "psi1"] #Carico folla
    + np.array(list_sigma[7])*psi.loc['temperatura', "psi1"] #temperatura
@@ -74,7 +113,8 @@ def combinazione(list_sigma, category = "A1_sfav"):
    sigma_qp = (np.array(list_sigma[0])*1 #G1
    + np.array(list_sigma[1])*1 #G2
    + np.array(list_sigma[2])*1 #Ritiro
-   + np.array(list_sigma[3])*psi.loc['tandem', "psi2"]#Carichi mobili concentrati
+
+   + np.array(list_sigma[4])*psi.loc['tandem', "psi2"]#Carichi mobili concentrati
    + np.array(list_sigma[5])*psi.loc['distribuiti', "psi2"] #Carichi mobili distribuiti
    + np.array(list_sigma[6])*psi.loc['folla', "psi2"] #Carico folla
    + np.array(list_sigma[7])*psi.loc['temperatura', "psi2"] #temperatura
@@ -300,30 +340,30 @@ def Sx_slab(bcls, hcls,  dictProp, condition = "positive"):
    Acls = bcls*hcls
 
    #calcolo del momento statico della soletta rispetto il baricentro della sezione composta
-   Sx_g1 = 0 # Acls *( dictProp["g1"]["Pg"][1]-hcls/2)
+   Sx_g1 = 0.0 # Acls *( dictProp["g1"]["Pg"][1]-hcls/2)
    Sx_g2 = Acls *( dictProp["g2"]["Pg"][1]-hcls/2)
    Sx_r = Acls *( dictProp["r"]["Pg"][1]-hcls/2)
    Sx_fat = Acls *( dictProp["mobili"]["Pg"][1]-hcls/2)
    Sx_ts = Acls *( dictProp["mobili"]["Pg"][1]-hcls/2)
    Sx_udl = Acls *( dictProp["mobili"]["Pg"][1]-hcls/2)
    Sx_folla = Acls *( dictProp["mobili"]["Pg"][1]-hcls/2)
-   Sx_t = Acls *( dictProp["c"]["Pg"][1]-hcls/2)
+   Sx_t = Acls *( dictProp["g2"]["Pg"][1]-hcls/2)
    Sx_c = Acls *( dictProp["c"]["Pg"][1]-hcls/2)
-   Sx_v = Acls *( dictProp["c"]["Pg"][1]-hcls/2)
+   Sx_v = Acls *( dictProp["g2"]["Pg"][1]-hcls/2)
 
    Sx_list = [Sx_g1, Sx_g2, Sx_r, Sx_fat, Sx_ts, Sx_udl, Sx_folla, Sx_t, Sx_c, Sx_v]
 
    # calcolo del braccio della forza interna
-   z_g1 = 0 # Acls *( dictProp["g1"]["Pg"][1]-hcls/2)
-   z_g2 = Sx_g2
-   z_r = Sx_r
-   z_fat = Sx_fat
-   z_ts = Sx_ts
-   z_udl = Sx_udl
-   z_folla = Sx_folla
-   z_t = Acls *Sx_t
-   z_c = Acls *Sx_c
-   z_v = Acls *Sx_v
+   z_g1 = 0.0 # Acls *( dictProp["g1"]["Pg"][1]-hcls/2)
+   z_g2 = dictProp["g2"]["Iy"]/Sx_g2
+   z_r = dictProp["r"]["Iy"]/Sx_r
+   z_fat = dictProp["mobili"]["Iy"]/Sx_fat
+   z_ts = dictProp["mobili"]["Iy"]/Sx_ts
+   z_udl = dictProp["mobili"]["Iy"]/Sx_udl
+   z_folla = dictProp["mobili"]["Iy"]/Sx_folla
+   z_t = dictProp["g2"]["Iy"]/Sx_t
+   z_c = dictProp["c"]["Iy"]/Sx_c
+   z_v = dictProp["g2"]["Iy"]/Sx_v
 
    z_list = [z_g1, z_g2, z_r, z_fat, z_ts, z_udl, z_folla, z_t, z_c, z_v]
 
@@ -456,9 +496,9 @@ def tension(dictProp, Sollecitazioni, hi_plot, condition = "positive"):
       N = Sollecitazioni[negList[7]]["N"]
       Mf = Sollecitazioni[negList[7]]["Mf"]
 
-   t_sigmaN = N*1000/dictProp["c"]["A"] #contributo per sola forza normale
-   hg = np.array(hi)+dictProp["c"]["Pg"][1]
-   t_sigmaMf = (Mf*1000**2/dictProp["c"]["Iy"])*hg #contributo per momento flettente
+   t_sigmaN = N*1000/dictProp["g2"]["A"] #contributo per sola forza normale
+   hg = np.array(hi)+dictProp["g2"]["Pg"][1]
+   t_sigmaMf = (Mf*1000**2/dictProp["g2"]["Iy"])*hg #contributo per momento flettente
    t_sigma = t_sigmaN + t_sigmaMf
    t_sigma[0], t_sigma[1] =  t_sigma[0]/ninf, t_sigma[1]/ninf
    t_sigma_plot = list(t_sigma) + [0.0, 0.0, t_sigma[0]]
@@ -486,9 +526,9 @@ def tension(dictProp, Sollecitazioni, hi_plot, condition = "positive"):
       N = Sollecitazioni[negList[9]]["N"]
       Mf = Sollecitazioni[negList[9]]["Mf"]
 
-   v_sigmaN = N*1000/dictProp["c"]["A"] #contributo per sola forza normale
-   hg = np.array(hi)+dictProp["c"]["Pg"][1]
-   v_sigmaMf = (Mf*1000**2/dictProp["c"]["Iy"])*hg #contributo per momento flettente
+   v_sigmaN = N*1000/dictProp["g2"]["A"] #contributo per sola forza normale
+   hg = np.array(hi)+dictProp["g2"]["Pg"][1]
+   v_sigmaMf = (Mf*1000**2/dictProp["g2"]["Iy"])*hg #contributo per momento flettente
    v_sigma = v_sigmaN + v_sigmaMf
    v_sigma[0], v_sigma[1] =  v_sigma[0]/ninf, v_sigma[1]/ninf
    v_sigma_plot = list(v_sigma) + [0.0, 0.0, v_sigma[0]]
@@ -953,21 +993,42 @@ st.markdown("""
             ##### 4.1) Verifica dei pioli (S.L.U.)
             """)
 
-Med_pos = Sollecitazione_list(updated_dict_soll, condition = "positive", cds= "Mf")
-Med_neg = Sollecitazione_list(updated_dict_soll, condition = "negative", cds= "Mf")
+#Med_pos = Sollecitazione_list(updated_dict_soll, condition = "positive", cds= "Mf")
+#Med_neg = Sollecitazione_list(updated_dict_soll, condition = "negative", cds= "Mf")
 
 
 Sx, zx = Sx_slab(Bcls, gapCls,  dictProp, condition = "positive")
 
-V_pioli_pos = np.array(Med_pos)/(np.array(zx)*1000)
-V_pioli_neg = np.array(Med_neg)/(np.array(zx)*1000)
+V_pioli_pos = np.array(Ved_pos)/(np.array(zx))
+V_pioli_neg = np.array(Ved_neg)/(np.array(zx))
+V_pioli_pos[0]= 0.0
+V_pioli_neg[0]= 0.0
 
-st.write(V_pioli_pos)
-st.write(V_pioli_neg)
+Vpioli_comb_pos = combinazione(list(V_pioli_pos), category = "A1_sfav")
+Vpioli_comb_neg = combinazione(list(V_pioli_neg), category = "A1_sfav")
+st.write(Vpioli_comb_pos)
+st.write(Vpioli_comb_neg)
 
-Vpioli_slu_pos = combinazione(V_pioli_pos, category = "A1_sfav")
-Vpioli_slu_neg = combinazione(V_pioli_neg, category = "A1_sfav")
+# Esempio di utilizzo
+d = 16  # mm
+ft = 450  # MPa
+fck = 30  # MPa
+Ec = 30000  # MPa
+hsc = 100  # mm
 
+nfp = 1 #numero file di pioli
+s_pioli = 200 #passo pioli in un metro
+nPioli_tot = (1000/s_pioli)*nfp
+
+resPiolo = np.min(Resistenza_Piolo(d, ft, fck, Ec, hsc)).real
+st.write(resPiolo)
+
+resTotalePioli = resPiolo*nPioli_tot
+st.write(resTotalePioli)
+
+## VERIFICA ALLO SLU
+dc_pioli_slu = np.abs(Vpioli_comb_pos[0]/resTotalePioli)
+st.write(dc_pioli_slu)
 
 
 st.markdown("""   
